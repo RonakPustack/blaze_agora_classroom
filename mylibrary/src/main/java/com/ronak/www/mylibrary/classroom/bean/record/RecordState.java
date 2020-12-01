2ffbe062c7fb1a3515f7b7d41a19a55f2b770437
RonakPustack <ronak@pustack.com>
2020-12-01 13:28:13 +0530

    Added "Test myLibrary"

---
+++ mylibrary/src/main/java/com/ronak/www/mylibrary/classroom/bean/record/RecordState.java
@@ -0,0 +1,6 @@
+package com.ronak.www.mylibrary.classroom.bean.record;
+
+public class RecordState {
+    public final static int START = 1;
+    public final static int END = 0;
+}
---
+++ mylibrary/src/main/java/com/ronak/www/mylibrary/service/bean/ResponseBody.java
@@ -0,0 +1,7 @@
+package com.ronak.www.mylibrary.service.bean;
+
+public class ResponseBody<T> extends io.agora.base.network.ResponseBody<String> {
+
+    public T data;
+
+}
