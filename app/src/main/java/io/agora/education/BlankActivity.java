36aa83f3f04d22f8a483f4da1d72be4c88f5399e
RonakPustack <ronak@pustack.com>
2020-12-02 15:23:29 +0530

    Layout changes in Blank Activity

--- app/src/main/java/io/agora/education/BlankActivity.java
+++ app/src/main/java/io/agora/education/BlankActivity.java
@@ -1,14 +1,23 @@
 package io.agora.education;
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import io.agora.education.base.BaseActivity;
 
-public class BlankActivity extends AppCompatActivity {
+public class BlankActivity extends BaseActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blank);
+    protected int getLayoutResId() {
+        return R.layout.activity_blank;
     }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
 }
